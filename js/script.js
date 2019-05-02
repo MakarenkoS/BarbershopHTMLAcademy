dade889a66f0354c938a83c0610c7ccbb8ed93fd
Sid <1@2.ru>
2019-05-02 12:56:48 +0400

    Верстка двух блоков адаптивная

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click",function(){
+	if (navMain.classList.contains("main-nav--opened")) {
+		navMain.classList.remove("main-nav--opened");
+		navMain.classList.add("main-nav--closed");
+	} else {
+		navMain.classList.remove("main-nav--closed");
+		navMain.classList.add("main-nav--opened");
+	}
+}) 
+
